db2cddc07f738efa932d41a82ff3cd3353ec0afd
Tkl02 <leonardofaustino0@gmail.com>
2025-10-27 16:41:33 -0300

    feat: vercel implementation update

--- src/middlewares/cron.middleware.ts
+++ src/middlewares/cron.middleware.ts
@@ -1,15 +1,15 @@
 import type { Request, Response, NextFunction } from "express"
 
 export function authCron(req: Request, res: Response, next: NextFunction) {
-    const authHeader = req.headers.authorization
     const cronSecret = process.env.CRON_SECRET
+    const { secret } = req.query;
 
     if (!cronSecret) {
         console.error('CRON_SECRET não esta configurado no .env')
         return res.status(500).json({ error: 'Configuração do servidor incompleta.' })
     }
 
-    if (authHeader === `Bearer ${cronSecret}`) {
+    if (secret === cronSecret) {
         next()
     } else {
         return res.status(401).json({ error: 'Não autorizado' })
